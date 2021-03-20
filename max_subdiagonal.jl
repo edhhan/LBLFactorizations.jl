98808595cab72431578414a45017813486499e52
edhhan <edward_hannan@hotmail.com>
2021-03-20 16:55:39 -0400

    spaces in max_subdiagonal.jl

--- max_subdiagonal.jl
+++ max_subdiagonal.jl
@@ -1,21 +1,23 @@
 using LinearAlgebra
 
+"""
+"""
 function max_subdiagonal(A::AbstractMatrix, column_index :: Number)
     #n is the number of rows of matrix A
-    n=size(A,1)
-    max_value=0
+    n = size(A,1)
+    max_value = 0
     #Return nothing if no subdiagonal values possible
     if (column_index >= n)
-        max_value=0
-        max_value_index=0
+        max_value = 0
+        max_value_index = 0
     else
         for i in column_index+1:n
             #Present value
-            v=abs(A[i,column_index])
+            v = abs(A[i,column_index])
             
-            if(v>max_value) 
-                max_value=v
-                max_value_index=i
+            if(v > max_value) 
+                max_value = v
+                max_value_index = i
             end
 
         end   
