97e7b13d9f86272dc1f14ef8847fd507b4847fc8
edhhan <edward_hannan@hotmail.com>
2021-03-26 14:33:43 -0400

    move lbl tests to tests.jl file

--- lbl.jl
+++ lbl.jl
@@ -156,36 +156,3 @@ function lbl(A::Hermitian{T}; strategy::String="rook") where T
     return F
 end
 
-# Test
-
-#=
-using Test
-@testset begin
-
-    for _ = 1:4
-    
-        for n = 4:100
-            A = Hermitian(rand(n,n).*100)
-            
-            F = lbl(A, strategy="rook")
-            @test norm(A - F.L*F.B*F.L') ≤ sqrt(eps()) * norm(A)
-
-            #PAPT = PermuteMatrix(A, F.pivot_array)
-            #@test norm(PAPT - F.L*F.B*F.L') ≤ sqrt(eps()) * norm(PAPT)
-        end
-    
-    end
-end
-=#
-
-A_assignement =[1 2 3 4 ; 2 5 13 28 ; 3 13 55 131 ; 4 28 131 270]
-A_assignement = Hermitian(A_assignement)
-F = lbl(A_assignement, strategy="bkaufmann")
-display(F.L)
-display(F.B)
-
-
-
-
-#@test norm(A_assignement - F.L*F.B*F.L') ≤ sqrt(eps()) * norm(A)
-
--- tests.jl
+++ tests.jl
@@ -1,10 +1,11 @@
 using LinearAlgebra
 include("bparlett.jl")
 include("bkaufmann.jl")
+include("lbl.jl")
 
-n=5
-
-A=rand(n,n)*1000+rand(n,n)+rand(n,n)+rand(n,n)+rand(n,n)+rand(n,n)-500*I
+#=
+n = 5
+A = rand(n,n)*1000+rand(n,n)+rand(n,n)+rand(n,n)+rand(n,n)+rand(n,n)-500*I
 B=Symmetric(A)
 
 pivots, dimension = bparlett(B)
@@ -12,3 +13,39 @@ pivots, dimension = bkaufmann(B)
 
 display(B)
 print(pivots,dimension)
+=#
+
+
+# Test
+
+#=
+using Test
+@testset begin
+
+    for _ = 1:4
+    
+        for n = 4:100
+            A = Hermitian(rand(n,n).*100)
+            
+            F = lbl(A, strategy="rook")
+            @test norm(A - F.L*F.B*F.L') ≤ sqrt(eps()) * norm(A)
+
+            #PAPT = PermuteMatrix(A, F.pivot_array)
+            #@test norm(PAPT - F.L*F.B*F.L') ≤ sqrt(eps()) * norm(PAPT)
+        end
+    
+    end
+end
+=#
+
+A_assignement =[1 2 3 4 ; 2 5 13 28 ; 3 13 55 131 ; 4 28 131 270]
+A_assignement = Hermitian(A_assignement)
+F = lbl(A_assignement, strategy="bkaufmann")
+display(F.L)
+display(F.B)
+
+
+
+
+#@test norm(A_assignement - F.L*F.B*F.L') ≤ sqrt(eps()) * norm(A)
+
