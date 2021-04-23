24eb28358707fbe676b49c9167b06c7ca0b1db77
Kardore1 <58898185+Kardore1@users.noreply.github.com>
2021-04-23 11:06:52 -0400

    English comments

--- lbl_solve.jl
+++ lbl_solve.jl
@@ -1,11 +1,14 @@
 include("LBL_structure.jl")
 
 using LinearAlgebra
-
+#Solving the Bx=b system using precalculated inverses of the blocks of B, B_inv[i][2] contains the index of
+# position of the block inverses in the block matrix B.d
+#To parallelize
 function solve_block_diagonal(B_inv::Array{Any,1}, b::AbstractVector)
     n=length(B_inv)
     for i=1:n
         dim=size(B_inv[i][1],1)
+        #Multiplying the member for the right (b) by the inverse of the blocks of B
         b[B_inv[i][2]:B_inv[i][2]+dim-1]=B_inv[i][1]*b[B_inv[i][2]:B_inv[i][2]+dim-1]
     end
     return b
--- tests/test_solver.jl
+++ tests/test_solver.jl
@@ -6,14 +6,14 @@ using ProfileSVG
 include("../lbl.jl")
 include("../lbl_solve.jl")
 
-n=10000
+n=1000
 A = Hermitian(rand(Float64, n,n).*100)
-ProfileSVG.@profview  F = lbl(A, "rook")
-b=rand(n).*100
+ProfileSVG.@profview  lbl(A, "rook")
+##b=rand(n).*100
 
 
 #ProfileSVG.@profview lbl_solve(F, b)
-println("DONE")
+#println("DONE")
 #ProfileSVG.@profview lbl_solve(F, b)
 #time1 = @elapsed x1=lbl_solve(F, b)
 #time2= @elapsed x2=A\b
